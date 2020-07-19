c3d8d00cb2fe8d86a2edfff249e8142bc3866967
Aalap Patel <aalappatel07@gmail.com>
2020-07-19 17:33:58 -0400

    add readme and implement 30min and 24 hour intervals for checks

---
+++ README.md
@@ -0,0 +1,20 @@
+# scrappy
+This is a Python script that allows a user to enter in URLs of job boards they want to watch as well as
+the keywords or job titles they are seeking by scraping the pages with BeautifulSoup. The script will run through all of the URLs and check if there are any matches for those keywords on that page. 
+
+Notification options
+* Text message using Twilio API
+* Email
+
+If a match is found on a given URL, the app will send a notification immediately. It will then wait 24 hours before
+sending another one as an additional check/reminder. If there are no matches, the page will be checked again at
+30 minute intervals.
+
+# Inspiration
+Making the job searching process easier! This can be utilized for any careers page that does not have a mailing
+list of its own to notify candidates of future opportunities and reduces the amount of manual checking required to keep
+up with new listings.
+
+# Note
+All credentials must be provided by the user. This includes email address and email authentication key as well as Twilio API
+sid, auth token, and phone numbers.
--- main.py
+++ main.py
@@ -4,55 +4,52 @@ import time
 from bs4 import BeautifulSoup
 from twilio.rest import Client
 
+# Insert all job board urls here
+urls = ["https://liveramp.com/careers/open-positions/"]
 
 while True:
-    url = "https://liveramp.com/careers/open-positions/"
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
-    response = requests.get(url, headers=headers)
-    soup = BeautifulSoup(response.text, "lxml")
-    
-    # Insert desired job title here
-    job = "intern"
-    if str(soup).find(job) == -1:
-        time.sleep(60)
-        continue
-
-    else:
-        # Text message version
-        account_sid = "SID"
-
-
-        auth_token  = "TOKEN"
-
-        client = Client(account_sid, auth_token)
-
-        message = client.messages.create(
-            to="+YOUR_NUMBER",
-            from_="+12058595480",
-            body="There is a new match for \"" + job + "\" at " + url)
-
-
-        # Email version
-
-        # Message to be sent upon finding job
-        msg = """\
-Subject: [scrappy notification]: new intern listing
-
-There is a new match for """ + "\"" + job + "\"" + " at " + url
+    for url in urls:
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+        response = requests.get(url, headers=headers)
+        soup = BeautifulSoup(response.text, "lxml")
         
-        # Setup gmail server
-        server = smtplib.SMTP('smtp.gmail.com', 587)
-        server.starttls()
+        # Insert desired job title here
+        job = "intern"
+        if str(soup).find(job) == -1:
+            time.sleep(1800) # Try again half an hour later
+            continue
 
-        # Sensitive info for logging into server and to/from emails
-        fromaddr = 'EMAIL'
-        toaddrs = ['EMAIL']
-        server.login("EMAIL", "KEY")
-        
+        else:
+            # Text messages 
+            account_sid = "INSERT_SID"
+            auth_token  = "INSERT_TOKEN"
+            client = Client(account_sid, auth_token)
+            message = client.messages.create(
+                to="INSERT_NUMBER",
+                from_="TWILIO_NUMBER",
+                body="There is a new match for \"" + job + "\" at " + url)
 
-        server.sendmail(fromaddr, toaddrs, msg)
-        server.quit()
+            # Email notifications
 
-        break
+            # Message to be sent upon finding job
+            msg = """\
+Subject: [scrappy notification]: Job listing matching """ + job + " has been found" + """
+
+There is a match for """ + "\"" + job + "\"" + " at " + url
+            
+            # Setup gmail server
+            server = smtplib.SMTP('smtp.gmail.com', 587)
+            server.starttls()
+
+            # Email info for logging into server and to/from emails
+            fromaddr = 'YOUR_EMAIL'
+            toaddrs = ['YOUR_EMAILS']
+            server.login("YOUR_EMAIL", "YOUR_KEY")
+
+            server.sendmail(fromaddr, toaddrs, msg)
+            server.quit()
+            time.sleep(86400) # Send a reminder/check again 24 hours later
+
+            continue
 
